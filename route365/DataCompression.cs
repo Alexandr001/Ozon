975348ac5cdd43a562c8fe2344dd9e084ab1daf2
Александр <aleksandr.popov@osinit.com>
2024-01-29 17:45:19 +0300

    Решена задачка с компрессией данных

---
+++ route365/DataCompression.cs
@@ -0,0 +1,64 @@
+﻿namespace route365;
+
+class DataCompression
+{
+    public static void Main()
+    {
+        var numberOfSet = int.Parse(Console.ReadLine()!);
+        var arrayResults = new int[numberOfSet][];
+        for (var i = 0; i < numberOfSet; i++)
+        {
+            _ = int.Parse(Console.ReadLine()!);
+            var array = Console.ReadLine()!.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            arrayResults[i] = Execute(array);
+        }
+
+        foreach (var result in arrayResults)
+        {
+            Console.WriteLine(result.Length);
+            Console.WriteLine(string.Join(' ', result));
+        }
+    }
+
+    static int[] Execute(int[] arr)
+    {
+        var results = new List<int>();
+        for (var i = 0; i < arr.Length; i++)
+        {
+            bool? isIncrease = null;
+            results.Add(arr[i]);
+
+            if (arr.ElementAtOrDefault(i) - arr.ElementAtOrDefault(i + 1) == 1)
+            {
+                isIncrease = false;
+            }
+            else if (arr.ElementAtOrDefault(i) - arr.ElementAtOrDefault(i + 1) == -1)
+            {
+                isIncrease = true;
+            }
+            else
+            {
+                results.Add(0);
+                continue;
+            }
+            
+            for (var j = i + 1; j <= arr.Length; j++)
+            {
+                if (j != arr.Length && arr[j - 1] - arr[j] == -1 && isIncrease == true)
+                {
+                    continue;
+                }
+                if (j != arr.Length && arr[j - 1] - arr[j] == 1 && isIncrease == false)
+                {
+                    continue;
+                }
+                
+                results.Add(arr[j - 1] - arr[i]);
+                i = j - 1;
+                break;
+            }
+        }
+        return results.ToArray();
+    }
+}
